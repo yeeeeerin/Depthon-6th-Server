e9c31dba465eb88f9877946c49c0daead2c43cec
yerin <hepari2154@naver.com>
2019-05-04 20:51:49 +0900

    feat swagger 설정 추가

---
+++ src/main/java/com/example/depthon3hangshi/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package com.example.depthon3hangshi.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+}
---
+++ src/main/java/com/example/depthon3hangshi/config/WebConfig.java
@@ -0,0 +1,5 @@
+package com.example.depthon3hangshi.config;
+
+
+public class WebConfig {
+}
