6d142a80a5facc0e82537ac65fb54635af00c786
yerin <hepari2154@naver.com>
2019-05-04 17:09:49 +0900

    project init

---
+++ src/main/java/com/example/depthon3hangshi/Depthon3hangshiApplication.java
@@ -0,0 +1,13 @@
+package com.example.depthon3hangshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Depthon3hangshiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Depthon3hangshiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/depthon3hangshi/Depthon3hangshiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.depthon3hangshi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Depthon3hangshiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
